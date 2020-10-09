7cb70a1a95c48f76cf763eac948c1943ccb6e962
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-09 16:39:15 +0200

    implement update author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -141,10 +141,56 @@ exports.author_delete_post = function(req, res, next) {
 
 // Display Author update form on GET.
 exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+    Author.findById(req.params.id, function (err, author) {
+        if (err) { return next(err); }
+        if (author == null) { // No results.
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Success.
+        res.render('author_form', { title: 'Update Author', author: author });
+
+    });
 };
 
 // Handle Author update on POST.
 exports.author_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
+    // Validate and sanitise fields.
+    body('first_name').isLength({ min: 1 }).trim().escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').isLength({ min: 1 }).trim().escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create an Author object with escaped and trimmed data.
+        var author = new Author(
+            {
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death
+            });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('author_form', { title: 'Update Author', author: author, errors: errors.array() });
+            return;
+        }
+        else {
+           // Data from form is valid. Update the record.
+           Author.findByIdAndUpdate(req.params.id, author, {}, function (err, theauthor) {
+            if (err) { return next(err); }
+            // Successful - redirect to genre detail page.
+            res.redirect(theauthor.url);
+        });
+        }
+    }
 };
