6e92894052f2e8ca31774f93412c1f1b0b135624
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-09 14:14:18 +0200

    implement delete genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -97,12 +97,49 @@ exports.genre_create_post =  [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+  async.parallel({
+    genre: function(callback) {
+        Genre.findById(req.params.id).exec(callback)
+    },
+    genre_books: function(callback) {
+      Book.find({ 'genre': req.params.id }).exec(callback)
+    },
+}, function(err, results) {
+    if (err) { return next(err); }
+    if (results.genre==null) { // No results.
+        res.redirect('/catalog/genres');
+    }
+    // Successful, so render.
+    res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
+});
 };
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete POST');
+  async.parallel({
+    author: function(callback) {
+      Genre.findById(req.body.genreid).exec(callback)
+    },
+    genre_books: function(callback) {
+      Book.find({ 'author': req.body.genreid }).exec(callback)
+    },
+}, function(err, results) {
+    if (err) { return next(err); }
+    // Success
+    if (results.genre_books.length > 0) {
+        // Genre has books. Render in same way as for GET route.
+        res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
+        return;
+    }
+    else {
+        // Genre has no books. Delete object and redirect to the list of genres.
+        Genre.findByIdAndRemove(req.body.genreid, function deleteAuthor(err) {
+            if (err) { return next(err); }
+            // Success - go to genre list
+            res.redirect('/catalog/genres')
+        })
+    }
+});
 };
 
 // Display Genre update form on GET.
